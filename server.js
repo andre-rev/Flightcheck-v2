3a4c81e7d45041820ae349519323f27e91c84041
andre-rev <andre@revenuepilots.de>
2025-06-25 10:46:00 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,9 +2,13 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 
+const evaluateRoute = require("./routes/evaluate"); // <== NEU
+
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+app.use("/api/evaluate", evaluateRoute); // <== NEU
 
 app.get("/", (req, res) => {
   res.send("Flightcheck 2.0 läuft ✅");
